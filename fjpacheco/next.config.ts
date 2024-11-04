d11c3d8b1bc6d2a312bb7ce327c145622929e032
Federico Pacheco <fpacheco@fi.uba.ar>
2024-11-04 10:46:59 -0300

    add test with next deployed in gh pages

---
+++ fjpacheco/next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  basePath: "/fjpacheco",
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
+  
+};
+
+export default nextConfig;
